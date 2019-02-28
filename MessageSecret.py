c2107e703bd9cbf85b001312baaf4f661f72e8e8
willemecloutier26 <willemecloutier@hotmail.com>
2019-02-28 16:22:43 -0500

    variable!

--- MessageSecret.py
+++ MessageSecret.py
@@ -1,3 +1,18 @@
 message = input('please enter your message').lower()
 key = (int)(input('please enter a number between 1 and  25'))
+if key < 1 :
+	key = 1
+elif key >25:
+	key = 25
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+newmessage =''
+
+for character in message:
+	if character in alphabet:
+		position = alphabet.find(character)
+		newposition = (position + key) % 26
+		newmessage += alphabet[ newposition ]
+	else :
+		newmessage += character
+print( newmessage )
